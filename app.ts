6275cd8dcf13b85cfee1482969b03fa65c0b4cb5
Aditya <adityasingh220598@gmail.com>
2022-09-25 11:02:54 +0530

    Adding REST routes with typescript

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import TodoRoutes from "./routes/todos";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(TodoRoutes);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
