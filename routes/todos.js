a76fb122f7d372c04794a6a9bc02ac203e38d450
Aditya <adityasingh220598@gmail.com>
2022-09-25 20:18:31 +0530

    Introducing type aliases for req.body and req.params

--- routes/todos.js
+++ routes/todos.js
@@ -7,25 +7,29 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos });
 });
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
 });
 router.put("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const body = req.body;
+    const params = req.params;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex((todo) => todo.id === todoId);
     if (todoIndex < 0) {
         // Send error as a response
         return res.status(404).json({ message: "Could not find the post" });
     }
-    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
     res.status(200).json({ message: "Updated!", updatedTodo: todos[todoIndex] });
 });
 router.delete("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex((todo) => todo.id === todoId);
     if (todoIndex < 0) {
         // Send error as a response
