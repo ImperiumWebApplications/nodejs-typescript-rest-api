8d7342084d0b3da6679e83d1b64f707505e09f1b
Aditya <adityasingh220598@gmail.com>
2022-09-25 12:48:38 +0530

    Updating posts route with typescript

--- routes/todos.ts
+++ routes/todos.ts
@@ -18,4 +18,14 @@ router.post("/todo", (req, res, next) => {
   res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
 });
 
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+  todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+  res.status(200).json({ message: "Updated!", updatedTodo: todos[todoIndex] });
+});
+
 export default router;
